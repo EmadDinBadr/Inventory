943379ea6d23304aff33f6ee37880b73dee634e7
Emad Din Badr Din <emadin9910@outlook.com>
2025-11-24 23:51:08 +0200

    Add project files.

---
+++ Inventory.Application/Features/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory.Application.Features.Products.Commands.CreateProduct
+{
+    public record CreateProductCommand
+    {
+    }
+}
